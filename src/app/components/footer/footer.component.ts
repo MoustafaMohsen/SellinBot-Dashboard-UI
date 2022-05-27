5ec745d815546641c05d5eb76b0bfafc153f1bb7
hiphusk <hiphusk@gmail.com>
2022-05-27 05:21:05 +0200

    footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,16 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  selector: "app-footer",
+  templateUrl: "./footer.component.html",
 })
 export class FooterComponent implements OnInit {
-  test : Date = new Date();
-  
-  constructor() { }
+  date = new Date().getFullYear();
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit(): void {}
 }
