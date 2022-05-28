4c5adfb81465dceeb23637617269206226bd6c90
hiphusk <hiphusk@gmail.com>
2022-05-28 22:23:49 +0200

    sidebar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit } from "@angular/core";
-import { StylesService } from "src/app/services/styles.service";
+import { AfterViewInit, Component, OnInit } from "@angular/core";
+import { ModalService } from "src/app/services/modal.service";
 
 @Component({
   selector: "app-sidebar",
   templateUrl: "./sidebar.component.html",
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, AfterViewInit {
   collapseShow = "hidden";
+
   constructor() {}
 
-  ngOnInit() {
-    // setup modal
-    let modal = new StylesService(".sidebar-modal","sidebar-modal-open", "sidebar-modal-close")
+  modal:ModalService;
+  ngAfterViewInit(): void {
+    this.modal = new ModalService(".sidebar-modal")
   }
+
+  ngOnInit() {}
   toggleCollapseShow(classes) {
     this.collapseShow = classes;
   }
+  openModal(){    
+    this.modal.openModal()
+  }
+  
 }
