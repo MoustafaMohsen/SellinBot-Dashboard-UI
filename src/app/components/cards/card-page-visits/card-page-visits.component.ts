5454a4561cee0bfc74a7d887a1dbf46eb094d219
hiphusk <hiphusk@gmail.com>
2022-05-28 22:24:22 +0200

    Add product modal

--- src/app/components/cards/card-page-visits/card-page-visits.component.ts
+++ src/app/components/cards/card-page-visits/card-page-visits.component.ts
@@ -1,11 +1,28 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit , AfterViewInit } from "@angular/core";
+import { ModalService } from "src/app/services/modal.service";
 
 @Component({
   selector: "app-card-page-visits",
   templateUrl: "./card-page-visits.component.html",
 })
-export class CardPageVisitsComponent implements OnInit {
+export class CardPageVisitsComponent implements OnInit, AfterViewInit {
+  collapseShow = "hidden";
+
   constructor() {}
 
+  modal:ModalService;
+  modal2:ModalService;
+
   ngOnInit(): void {}
+  
+  ngAfterViewInit(): void {
+    this.modal = new ModalService(".add-product-modal")
+    this.modal2 = new ModalService(".edit-product-modal")
+  }
+
+  toggleCollapseShow(classes) {
+    this.collapseShow = classes;
+  }
+  
 }
+
