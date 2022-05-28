afeacac3f1d6e4b4e291170fdbc34c230d003b34
moustafamohsen <contact@moustafamohsen.com>
2022-05-28 22:41:38 +0200

    updated routing and deleted unrefrenced components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,11 +14,6 @@ import { TablesComponent } from "./views/admin/tables/tables.component";
 import { LoginComponent } from "./views/auth/login/login.component";
 import { RegisterComponent } from "./views/auth/register/register.component";
 
-// no layouts views
-import { IndexComponent } from "./views/index/index.component";
-import { LandingComponent } from "./views/landing/landing.component";
-import { ProfileComponent } from "./views/profile/profile.component";
-
 const routes: Routes = [
   // admin views
   {
@@ -34,7 +29,7 @@ const routes: Routes = [
   },
   // auth views
   {
-    path: "auth",
+    path: "",
     component: AuthComponent,
     children: [
       { path: "login", component: LoginComponent },
@@ -43,9 +38,6 @@ const routes: Routes = [
     ],
   },
   // no layout views
-  { path: "profile", component: ProfileComponent },
-  { path: "landing", component: LandingComponent },
-  { path: "", component: IndexComponent },
   { path: "**", redirectTo: "", pathMatch: "full" },
 ];
 
