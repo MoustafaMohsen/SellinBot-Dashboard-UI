7abcf69f52230be72528a67ce3a6488fa337e163
hiphusk <hiphusk@gmail.com>
2022-05-29 02:20:25 +0200

    Change admin layouts names

---
+++ src/app/layouts/admin/botcontrol/botcontrol.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-botcontrol",
+  templateUrl: "./botcontrol.component.html",
+})
+export class BotcontrolComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/layouts/admin/convos/convos.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-convos",
+  templateUrl: "./convos.component.html",
+})
+export class ConvosComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
