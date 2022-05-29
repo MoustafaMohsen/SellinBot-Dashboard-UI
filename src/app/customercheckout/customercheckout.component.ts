734c964a30b97999dcf963551e5f1d7797cc9366
hiphusk <hiphusk@gmail.com>
2022-05-29 02:21:43 +0200

    Customer Checkout Compnonent

---
+++ src/app/customercheckout/customercheckout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customercheckout',
+  templateUrl: './customercheckout.component.html',
+  styleUrls: ['./customercheckout.component.css']
+})
+export class CustomercheckoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
